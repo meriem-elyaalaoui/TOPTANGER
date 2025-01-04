e0f964acbf312c636980fcacba594d509d7262a1
meriem-elyaalaoui <elyaalaouimeriem1994@gmail.com>
2025-01-04 13:07:28 +0100

    Add files via upload

---
+++ documents/data/REFERENCE_2.js
@@ -0,0 +1,1 @@
+var json_REFERENCE_2 = {"type":"FeatureCollection","name":"REFERENCE_2","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1","NOM":"GARE TANGER VILLE","X":464929.0,"Y":574511.0},"geometry":{"type":"Point","coordinates":[-5.785984866907097,35.771528276798598]}}]}
